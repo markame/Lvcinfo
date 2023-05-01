d080faafca0628729cc3fdeab24e6b51a41e1964
markame <mr.markame@gmail.com>
2023-04-30 21:09:47 -0300

    imagem

--- Lvcinfo/Lvcinfo/ViewModels/PaginaInicialModel.cs
+++ Lvcinfo/Lvcinfo/ViewModels/PaginaInicialModel.cs
@@ -14,12 +14,17 @@ namespace Lvcinfo.ViewModels
         public Command listarRegistroCommand { get; set; }
         public Command sobreLvcCommmand { get; set; }
         public Command listarRegistroEncerradoCommand { get; set; }
+        public Command faleconoscoCommand { get; set; }
+        public Command sobreappCommand { get; set; }    
         public PaginaInicialModel()
         {
             novaNotificacaoCommand = new Command(async () => await novaNotificacaoClick());
             listarRegistroCommand = new Command(async () => await listarRegistroClick());
             listarRegistroEncerradoCommand = new Command(async () => await listarRegistroEncerradoClick());
             sobreLvcCommmand = new Command(async() => await sobreLvcClick());
+            faleconoscoCommand = new Command(async () => await faleConoscoClick());
+            sobreappCommand = new Command(async () => await sobreAppClick());
+            
 
         }
 
@@ -43,8 +48,14 @@ namespace Lvcinfo.ViewModels
             await Application.Current.MainPage.Navigation.PushAsync(new NovaNotificacao());
         }
 
-       
-
+        private async Task faleConoscoClick()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new FaleConosco());
+        }
+         private async Task sobreAppClick()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new SobreApp());
+        }
 
 
     }
---
+++ Lvcinfo/Lvcinfo/Views/SobreApp.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lvcinfo.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SobreApp : ContentPage
+	{
+		public SobreApp ()
+		{
+			InitializeComponent ();
+            Build.Text = "Build atual: "+GetCurrentBuild()+".";
+            
+            Versao.Text = "Versão atual: "+GetCurrentVersion()+".";
+		}
+
+        private static string GetCurrentBuild()
+        {
+            return VersionTracking.CurrentBuild;
+        }
+
+        private static string GetCurrentVersion()
+        {
+            return VersionTracking.CurrentVersion;
+        }
+    }
+}
